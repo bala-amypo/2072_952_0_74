e4ce9468a9a5598bb33b2226db38f73584e5fb18
bala-amypo <bala@amypo.in>
2025-12-17 09:11:09 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,23 +1,21 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.example.demo.service.StudentService;
-import com.example.demo.entity.Student;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Student;
+import com.example.demo.service.StudentService;
 
 @RestController
 public class StudentController {
 
     @Autowired
-    StudentService stdser;
+    private StudentService studentService;
 
     @PostMapping("/addStudent")
-    public Student addStudent(@RequestBody Student st) {
-        return stdser.postStudent(st);
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.postStudent(student);
     }
 }
